5be1d221420d6b2eec0cd2736521e1b30dd29be6
Cecy Flores <anacecyflores1@gmail.com>
2022-09-09 23:13:38 -0500

    renamed database

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost:27017/googlebooks',
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/worldview',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
