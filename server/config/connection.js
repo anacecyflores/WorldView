82da07afe3f43f6738f73c1cc71bd1bdae514d8d
Cecy Flores <anacecyflores1@gmail.com>
2022-09-13 22:03:28 -0500

    Connected to GraphQl '

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 mongoose.connect(
   process.env.MONGODB_URI ||
-    "mongodb+srv://FinalProject:northwestern>@worldview.hf9necs.mongodb.net/?retryWrites=true&w=majority",
+    "mongodb+srv://FinalProject:northwestern@worldview.hf9necs.mongodb.net/?retryWrites=true&w=majority",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
